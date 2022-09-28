2874490cd0811ec45c161aca547a2bcad4cab2f4
Ajay-526 <ajjaystyles@gmail.com>
2022-09-28 17:38:35 +0530

    sixth commit

--- components/CardContent.tsx
+++ components/CardContent.tsx
@@ -5,7 +5,7 @@ import StatasticsComponent from "./StatasticsComponent";
 
 function CardContent() {
   return (
-    <Box sx={{ minHeight: {xs: '200vh', sm:'125vh'}, bgcolor: "hsl(230, 25%, 95%)" }}>
+    <Box sx={{ minHeight: {xs: '210vh', sm:'125vh'}, bgcolor: "hsl(230, 25%, 95%)" }}>
       <Container maxWidth="lg">
         <Box sx={{ position: "relative", bottom: 50 }}>
           <SearchComponent />
